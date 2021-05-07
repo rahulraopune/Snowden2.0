eb87f88a52f77be5a9345a2d7feeb09bfe145a41
Shivam <sshivam95@gmail.com>
2021-05-07 18:41:09 +0200

    Add histogram and Bar charts

--- Snowden/plot.py
+++ Snowden/plot.py
@@ -1,20 +1,16 @@
 import json
-import numpy as np
-import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
 
 # Function to plot histogram per argument length
 # Here we plot the length of each argument
-def plot_hist_per_arg_length(hist):
+def plot_hist_per_arg_length():
     fig = plt.figure(figsize=(10, 6), dpi=100)
-    ax = plt.axes()
     plt.hist(hist_per_argument_length, density=False)
     plt.ylabel("Number of arguments")
     plt.xlabel("Length of arguments")
     plt.title("Histogram for length of arguments")
-    fig.savefig('Hist_per_argument_length.png',
-                bbox_inches="tight", dpi=150)
+    fig.savefig('Histogram.png', bbox_inches="tight", dpi=150)
 
 
 # Function to plot Bar Graph per category and  Stacked Bar Graph per topic
@@ -28,8 +24,7 @@ def plot_number_of_argument_graph(x, y, z, type_of_plot):
         plt.xlabel("Category")
         plt.title("Bar Graph for length of arguments per category")
         plt.bar(x, y)
-        fig.savefig('Bar_Graph_Num_arg_per_category.png',
-                    bbox_inches="tight", dpi=150)
+        fig.savefig('BarGraph_category.png', bbox_inches="tight", dpi=150)
     elif type_of_plot == 'Per Topic':
         ax.set_xticklabels(x, rotation=90)
         plt.xlabel("Topic")
@@ -37,8 +32,7 @@ def plot_number_of_argument_graph(x, y, z, type_of_plot):
         plt.bar(x, z, color='r')
         plt.bar(x, y, bottom=z, color='g')
         plt.legend(color_legend, loc=0)
-        fig.savefig('Bar_Graph_Num_arg_per_topic.png',
-                    bbox_inches="tight", dpi=150)
+        fig.savefig('BarGraph_topic.png', bbox_inches="tight", dpi=150)
 
 
 # Load contents of json file
@@ -82,18 +76,17 @@ for topic in range(0, len(json_data)):
     # Create a dictionary to store the number of arguments
     # per category eg dict = {'Category 1': 10, 'Category 2' : 20
     if json_data[topic]['category'] in hist_per_category.keys():
-        hist_per_category[json_data[topic]['category']] = hist_per_category.get(
-            json_data[topic]['category']) + num_args
+        hist_per_category[json_data[topic]['category']] = hist_per_category.get(json_data[topic]['category']) + num_args
     else:
         hist_per_category[json_data[topic]['category']] = num_args
     hist_plot_per_topic.append(num_args)
     hist_plot_per_category = list(hist_per_category.values())
 
 # Plot histogram per argument length
-plot_hist_per_arg_length(hist_per_argument_length)
-# Plot histogram for the number of arguements per category
+plot_hist_per_arg_length()
+# Plot histogram for the number of arguments per category
 plot_number_of_argument_graph(
     x=hist_per_category.keys(), y=hist_per_category.values(), z=0, type_of_plot='Per Category')
-# Plot histogram for the number of arguements per topic
+# Plot histogram for the number of arguments per topic
 plot_number_of_argument_graph(
     x=hist_per_topic.keys(), y=stack_bar_pro_topic, z=stack_bar_con_topic, type_of_plot='Per Topic')
